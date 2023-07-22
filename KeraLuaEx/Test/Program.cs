3ac41326464d98788d364c40b0aee7a7bc0b7c78
cepthomas <cepthomas@gmail.com>
2023-07-22 10:33:14 -0500

    Add test project.

---
+++ KeraLuaEx/Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using LuaXXX;
+//using LuaXXX.Test;
+
+namespace KeraLuaEx.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
