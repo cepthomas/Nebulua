95b2be442e8f1a872faca509289b819fefdefff2
cepthomas <cepthomas@gmail.com>
2023-12-30 11:17:54 -0600

    WIP.

--- c/luainteropwork.c
+++ c/luainteropwork.c
@@ -17,17 +17,24 @@
 
 // Definition of work functions for host functions called by lua.
 
+// Macro used to handle user syntax errors in the interop work functions.
+#define VALS(expr, s) luaL_error(l, "%s: %s", #expr, s)
+#define VALI(expr, i) luaL_error(l, "%s: %d", #expr, i)
+#define VALF(expr, f) luaL_error(l, "%s: %f", #expr, f)
+
+
 //--------------------------------------------------------//
-int luainteropwork_Log(int level, char* msg)
+int luainteropwork_Log(lua_State* l, int level, char* msg)
 {
-    return logger_Log(level, msg);
+    logger_Log(level, msg);
+    return 0;
 }
 
 
 //--------------------------------------------------------//
-int luainteropwork_SetTempo(int bpm)
+int luainteropwork_SetTempo(lua_State* l, int bpm)
 {
-    VALIDATE(bpm >= 30 && bpm <= 240);
+    VALI(bpm >= 30 && bpm <= 240, bpm);
 
     double sec_per_beat = 60.0 / bpm;
     double msec_per_subbeat = 1000 * sec_per_beat / SUBEATS_PER_BAR;
@@ -40,71 +47,70 @@ int luainteropwork_SetTempo(int bpm)
 
 
 //--------------------------------------------------------//
-int luainteropwork_CreateChannel(const char* sys_dev_name, int chan_num, int patch)
+int luainteropwork_CreateChannel(lua_State* l, const char* sys_dev_name, int chan_num, int patch)
 {
     int hndchan = 0;
 
-    VALIDATE(sys_dev_name != NULL);
-    VALIDATE(chan_num >= 1 && chan_num <= NUM_MIDI_CHANNELS);
-    VALIDATE(patch >= 0 && patch < MIDI_VAL_MAX);
+    VALS(sys_dev_name != NULL, sys_dev_name);
+    VALI(chan_num >= 1 && chan_num <= NUM_MIDI_CHANNELS, chan_num);
+    VALI(patch >= 0 && patch < MIDI_VAL_MAX, patch);
 
     midi_device_t* pdev = devmgr_GetDeviceFromName(sys_dev_name);
-    VALIDATE(pdev);
+    VALS(pdev != NULL, sys_dev_name);
 
     hndchan = devmgr_GetChannelHandle(pdev, chan_num);
-    VALIDATE(hndchan > 0);
+    VALI(hndchan > 0, chan_num);
 
     // Send patch now.
     int short_msg = (chan_num - 1) + MIDI_PATCH_CHANGE + (patch << 8);
-    int ret = midiOutShortMsg(pdev->hnd_out, short_msg);
+    int mstat = midiOutShortMsg(pdev->hnd_out, short_msg);
+    VALS(mstat == MMSYSERR_NOERROR, common_MidiStatusToString(mstat));
 
     return hndchan;
 }
 
 
 //--------------------------------------------------------//
-int luainteropwork_SendNote(int hndchan, int note_num, double volume, double dur) // TODO2 if dur>0 add note off
+int luainteropwork_SendNote(lua_State* l, int hndchan, int note_num, double volume, double dur) // TODO1 if dur>0 add note off
 {
-    int ret = LUA_OK;
-
-    VALIDATE(hndchan > 0);
-    VALIDATE(note_num >= 0 && note_num < MIDI_VAL_MAX);
-    VALIDATE(volume >= 0.0 && volume <= 1.0);
-    VALIDATE(dur >= 0.0 && dur <= 100.0);
+    VALI(hndchan > 0, hndchan);
+    VALI(note_num >= 0 && note_num < MIDI_VAL_MAX, note_num);
+    VALF(volume >= 0.0 && volume <= 1.0, volume);
+    VALF(dur >= 0.0 && dur <= 100.0, dur);
 
     midi_device_t* pdev = devmgr_GetOutputDeviceFromChannelHandle(hndchan);
-    VALIDATE(pdev);
+    VALI(pdev != NULL, hndchan);
 
     int chan_num = devmgr_GetChannelNumber(hndchan);
-    VALIDATE(chan_num >= 1 && chan_num <= NUM_MIDI_CHANNELS);
+    VALI(chan_num >= 1 && chan_num <= NUM_MIDI_CHANNELS, chan_num);
 
     int cmd = volume == 0.0 ? MIDI_NOTE_OFF : MIDI_NOTE_ON;
     int velocity = (int)(volume * MIDI_VAL_MAX);
     int short_msg = (chan_num - 1) + cmd + ((byte)note_num << 8) + ((byte)velocity << 16);
-    ret = midiOutShortMsg(pdev->hnd_out, short_msg);
+    int mstat = midiOutShortMsg(pdev->hnd_out, short_msg);
+    VALS(mstat == MMSYSERR_NOERROR, common_MidiStatusToString(mstat));
 
-    return ret;
+    return 0;
 }
 
 
 //--------------------------------------------------------//
-int luainteropwork_SendController(int hndchan, int ctlr, int value)
+int luainteropwork_SendController(lua_State* l, int hndchan, int ctlr, int value)
 {
-    int ret = LUA_OK;
-
-    VALIDATE(hndchan > 0);
-    VALIDATE(ctlr >= 0 && ctlr < MIDI_VAL_MAX);
-    VALIDATE(value >= 0 && value < MIDI_VAL_MAX);
+    VALI(hndchan > 0, hndchan);
+    VALI(ctlr >= 0 && ctlr < MIDI_VAL_MAX, ctlr);
+    VALI(value >= 0 && value < MIDI_VAL_MAX, value);
 
     midi_device_t* pdev = devmgr_GetOutputDeviceFromChannelHandle(hndchan);
-    VALIDATE(pdev);
+    VALI(pdev != NULL, hndchan);
 
     int chan_num = devmgr_GetChannelNumber(hndchan);
-    VALIDATE(chan_num >= 1 && chan_num <= NUM_MIDI_CHANNELS);
+    VALI(chan_num >= 1 && chan_num <= NUM_MIDI_CHANNELS, hndchan);
 
     int cmd = MIDI_CONTROL_CHANGE;
     int short_msg = (chan_num - 1) + cmd + ((byte)ctlr << 8) + ((byte)value << 16);
-    ret = midiOutShortMsg(pdev->hnd_out, short_msg);
+    int mstat = midiOutShortMsg(pdev->hnd_out, short_msg);
+    VALS(mstat == MMSYSERR_NOERROR, common_MidiStatusToString(mstat));
 
-    return ret;
+    return 0;
 }
--- c/luainteropwork.h
+++ c/luainteropwork.h
@@ -7,21 +7,20 @@
 // Declaration of work functions for host functions called by lua.
 // See interop_spec.lua for api. TODO1 autogen this and/or make an md of api.
 
-#define VALIDATE(expr) luainterop_SyntaxError(#expr)
 
 //--------------------------------------------------------//
-int luainteropwork_Log(int level, char* msg);
+int luainteropwork_Log(lua_State* l, int level, char* msg);
 
 //--------------------------------------------------------//
-int luainteropwork_SetTempo(int bpm);
+int luainteropwork_SetTempo(lua_State* l, int bpm);
 
 //--------------------------------------------------------//
-int luainteropwork_CreateChannel(const char* device, int chan_num, int patch);
+int luainteropwork_CreateChannel(lua_State* l, const char* device, int chan_num, int patch);
 
 //--------------------------------------------------------//
-int luainteropwork_SendNote(int hndchan, int notenum, double volume, double dur);
+int luainteropwork_SendNote(lua_State* l, int hndchan, int notenum, double volume, double dur);
 
 //--------------------------------------------------------//
-int luainteropwork_SendController(int hndchan, int ctlr, int value);
+int luainteropwork_SendController(lua_State* l, int hndchan, int ctlr, int value);
 
 #endif // LUAINTEROPWORK_H
