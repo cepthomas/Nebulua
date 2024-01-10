4fd671a3c26a4854fc6ae4d01a1976a866557b56
cepthomas <cepthomas@gmail.com>
2024-01-10 16:42:31 -0600

    Revert lost files.

---
+++ README.md
@@ -0,0 +1,71 @@
+# Nebulua
+An experimental version of Nebulator using Lua as the script flavor.
+
+Copy stuff from Nebulator\README.md
+
+
+
+field types:
+S = string
+N = number
+I = integer
+F = function
+M = map index(0-9)
+T = TableEx
+V = void
+B = boolean
+X = bar time(Number?)
+E = expression?
+
+
+---- sequences
+-- Graphical format:
+-- "|7-------|" is one beat with 8 subbeats
+-- note velocity is 1-9 (map) or - which means sustained
+-- note/chord, velocity/volume
+-- List format:
+-- times are beat.subbeat where beat is 0-N subbeat is 0-7
+-- note/chord, velocity/volume is 0.0 to 1.0, duration is 0.1 to N.7
+
+
+Reload:
+- https://stackoverflow.com/questions/2812071/what-is-a-way-to-reload-lua-scripts-during-run-time
+- https://stackoverflow.com/questions/9369318/hot-swap-code-in-lua
+
+
+
+
+# c_emb_lua xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+A minimal example of a C embedded executable with lua script processor.
+It demonstrates:
+- Coroutines - one state for the C side and one for the script side.
+- Calling script functions with args from the C side.
+- Calling C functions with args from the script side.
+- Simulated embedded system with a hardware level, CLI for control, and exec loop running everything.
+- Rudimentary error handling model - more required.
+
+# Build
+- Pure C99 which should compile anywhere, including small embedded systems - basically anywhere you can compile lua.
+- A VS Code workspace using mingw and CMake is supplied. Your PATH needs to include mingw.
+- Run build.cmd to make the executables.
+
+# Files
+- [Conventions](https://github.com/cepthomas/c_bag_of_tricks/blob/master/CONVENTIONS.md).
+- [Modular model](https://github.com/cepthomas/c_modular).
+- `c` folder:
+    - main.c - Entry stub calls exec.
+    - exec.c/h - Does all the top-level work.
+    - board.c/h - Interface to the (simulated) hardware.
+    - common.c/h - Misc time, strings, ...
+    - luainterop.c/h - Interfaces to call lua functions from C and to call C functions from lua.
+    - luautils.c/h - General purpose tools for probing lua stacks.
+- `lua` folder:
+    - demoapp.lua - Lua script for a simplistic multithreaded coroutine application. Uses luatoc.
+    - utils.lua - Used by demoapp.lua.
+- `lua-5.3.5` folder:
+    - lua source code for this application. Stock except where marked by `C_EMB_LUA`.
+
+# Licenses
+[This repo](https://github.com/cepthomas/c-emb-lua/blob/master/LICENSE)
+
+[Lua](https://github.com/cepthomas/c-emb-lua/blob/master/LUA-LICENSE)
---
+++ interop_spec.lua
@@ -0,0 +1,218 @@
+
+local M = {}
+
+M.config =
+{
+    lua_lib_name = "host_api",
+    -- add_refs = { "<abc.h>", },
+}
+
+-- Host calls lua.
+M.lua_export_funcs =
+{
+    {
+        lua_func_name = "setup",
+        host_func_name = "Setup",
+        description = "Called to initialize Nebulator stuff.",
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+    {
+        lua_func_name = "step",
+        host_func_name = "Step",
+        description = "Called every fast timer increment.",
+        args =
+        {
+            {
+                name = "bar",
+                type = "I",
+                description = "Which bar 0-N"
+            },
+            {
+                name = "beat",
+                type = "I",
+                description = "Which beat 0-3"
+            },
+            {
+                name = "subbeat",
+                type = "I",
+                description = "Which subbeat 0-7"
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+    {
+        lua_func_name = "input_note",
+        host_func_name = "InputNote",
+        description = "Called when input arrives.",
+        args =
+        {
+            {
+                name = "hndchan",
+                type = "I",
+                description = "Input channel handle"
+            },
+            {
+                name = "notenum",
+                type = "I",
+                description = "Note number"
+            },
+            {
+                name = "volume",
+                type = "N",
+                description = "Volume between 0.0 and 1.0."
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+    {
+        lua_func_name = "input_controller",
+        host_func_name = "InputController",
+        description = "Called when input arrives.",
+        args =
+        {
+            {
+                name = "hndchan",
+                type = "I",
+                description = "Input channel handle"
+            },
+            {
+                name = "controller",
+                type = "I",
+                description = "Specific controller id"
+            },
+            {
+                name = "value",
+                type = "I",
+                description = "Payload"
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+}
+
+-- Lua calls host.
+M.host_export_funcs =
+{
+
+    {
+        lua_func_name = "create_channel",
+        host_func_name = "CreateChannel",
+        description = "Create an in or out midi channel.",
+        args =
+        {
+            {
+                name = "device",
+                type = "S",
+                description = "Midi device name"
+            },
+            {
+                name = "channum",
+                type = "I",
+                description = "Midi channel number 1-16"
+            },
+            {
+                name = "patch",
+                type = "I",
+                description = "Midi patch number (output channel only)"
+            },
+        },
+        ret = { type = "I", description = "Channel handle or 0 if invalid" }
+    },
+
+    {
+        lua_func_name = "log",
+        host_func_name = "Log",
+        description = "Script wants to log something.",
+        args =
+        {
+            {
+                name = "level",
+                type = "I",
+                description = "Log level"
+            },
+            {
+                name = "msg",
+                type = "S",
+                description = "Log message"
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+    {
+        lua_func_name = "set_tempo",
+        host_func_name = "SetTempo",
+        description = "Script wants to change tempo.",
+        args =
+        {
+            {
+                name = "bpm",
+                type = "I",
+                description = "BPM"
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+    {
+        lua_func_name = "send_note",
+        host_func_name = "SendNote",
+        description = "If volume is 0 note_off else note_on. If dur is 0 send note_on with dur = 0.1 (for drum/hit).",
+        args =
+        {
+            {
+                name = "hndchan",
+                type = "I",
+                description = "Output channel handle"
+            },
+            {
+                name = "notenum",
+                type = "I",
+                description = "Note number"
+            },
+            {
+                name = "volume",
+                type = "N",
+                description = "Volume between 0.0 and 1.0"
+            },
+            {
+                name = "dur",
+                type = "N",
+                description = "Duration as bar.beat"
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+    {
+        lua_func_name = "send_controller",
+        host_func_name = "SendController",
+        description = "Send a controller immediately.",
+        args =
+        {
+            {
+                name = "hndchan",
+                type = "I",
+                description = "Output channel handle"
+            },
+            {
+                name = "controller",
+                type = "I",
+                description = "Specific controller"
+            },
+            {
+                name = "value",
+                type = "I",
+                description = "Payload."
+            },
+        },
+        ret = { type = "I", description = "LUA_STATUS" }
+    },
+
+}
+
+return M
