94e648e05f9702e3a635edd4e3d30103cc952e81
cepthomas <cepthomas@gmail.com>
2024-01-12 11:51:20 -0600

    Clean up.

--- test.cmd
+++ test.cmd
@@ -1,6 +1,5 @@
 
 echo off
-cls
 
 if not exist .\c\build_test mkdir .\c\build_test
 rem mkdir .\c\build_test
@@ -8,11 +7,21 @@ rem del /F /Q .\c\build_test\*.*
 
 pushd .\c\build_test
 
-:: Build the app.
+:: Build the c app.
 cmake -G "MinGW Makefiles" -DDO_TEST=1 ..
 make
 
-:: Run tests.
+:: Run c tests.
 nebulua
+
+popd
+
+:: Run lua tests.
+pushd lua\test\
+:: Set the lua path.
+set LUA_PATH=;;C:\Dev\repos\Lua\LuaBagOfTricks\?.lua;C:\Dev\repos\Audio\Nebulua\lua\?.lua;C:\Dev\repos\Audio\Nebulua\lua\test\?.lua;
+
+:: Run the unit tests. test_defs.lua  test_nebulua.lua
+lua C:\Dev\repos\Lua\LuaBagOfTricks\pnut_runner.lua test_nebulua.lua
 
 popd
