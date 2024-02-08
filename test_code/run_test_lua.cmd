a7700a18e461281a16bbd3f01b23d597edd66c4d
cepthomas <cepthomas@gmail.com>
2024-02-08 12:49:55 -0600

    Added local lua lib to eliminate dependencies.

--- test_code/run_test_lua.cmd
+++ test_code/run_test_lua.cmd
@@ -2,7 +2,7 @@
 echo off
 
 :: Run lua tests.  test_defs.lua  test_nebulua.lua
-set LUA_PATH=;;C:\Dev\repos\Lua\Nebulua\lua\?.lua;C:\Dev\repos\Lua\Nebulua\test_code\?.lua;C:\Dev\repos\Lua\LuaBagOfTricks\?.lua;
+set LUA_PATH=;;C:\Dev\repos\Lua\Nebulua\lua_code\?.lua;C:\Dev\repos\Lua\Nebulua\test_code\?.lua;C:\Dev\repos\Lua\LuaBagOfTricks\?.lua;
 rem lua C:\Dev\repos\Lua\LuaBagOfTricks\pnut_runner.lua  test_defs.lua
 rem lua C:\Dev\repos\Lua\LuaBagOfTricks\pnut_runner.lua  test_nebulua.lua
 
