5ee5c3a31bf64eb7c708b249e14173e7857ac77c
cepthomas <cepthomas@gmail.com>
2024-06-16 16:23:16 -0500

    WIP.

--- test/run_all.cmd
+++ test/run_all.cmd
@@ -1,18 +1,18 @@
 
 echo off
 
-pushd "CommandProc\bin\x64\Debug\net8.0-windows7.0"
+pushd "CommandProc\bin\x64\Debug\net8.0-windows"
 TestCommandProc.exe
 popd
 
-pushd "Core\bin\x64\Debug\net8.0-windows7.0"
+pushd "Core\bin\x64\Debug\net8.0-windows"
 TestCore.exe
 popd
 
-pushd "Interop\bin\x64\Debug\net8.0-windows7.0"
+pushd "Interop\bin\x64\Debug\net8.0-windows"
 TestInterop.exe
 popd
 
-pushd "Misc\bin\x64\Debug\net8.0-windows7.0"
+pushd "Misc\bin\x64\Debug\net8.0-windows"
 TestMisc.exe
 popd
