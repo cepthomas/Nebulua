36a41f032ac6ebf4c012a7fcaa0d7bf085844219
cepthomas <cepthomas@gmail.com>
2024-06-25 14:58:29 -0500

    Fixed settings.

---
+++ Common/UserSettings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Drawing.Design;
+using NAudio.Midi;
+using Ephemera.NBagOfTricks;
+using Ephemera.NBagOfTricks.Slog;
+
+
+namespace Nebulua.Common
+{
+    [Serializable]
+    public sealed class UserSettings : SettingsCore
+    {
+        #region Properties - persisted editable
+        [DisplayName("File Log Level")]
+        [Description("Log level for file write.")]
+        [Browsable(true)]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public LogLevel FileLogLevel { get; set; } = LogLevel.Trace;
+
+        [DisplayName("File Log Level")]
+        [Description("Log level for UI notification.")]
+        [Browsable(true)]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public LogLevel NotifLogLevel { get; set; } = LogLevel.Debug;
+
+        [DisplayName("Icon Color")]
+        [Description("The color used for button icons.")]
+        [Browsable(true)]
+        [JsonConverter(typeof(JsonColorConverter))]
+        public Color IconColor { get; set; } = Color.Purple;
+
+        [DisplayName("Control Color")]
+        [Description("The color used for active control surfaces.")]
+        [Browsable(true)]
+        [JsonConverter(typeof(JsonColorConverter))]
+        public Color ControlColor { get; set; } = Color.DodgerBlue;
+
+        [DisplayName("Selected Color")]
+        [Description("The color used for selected controls.")]
+        [Browsable(true)]
+        [JsonConverter(typeof(JsonColorConverter))]
+        public Color SelectedColor { get; set; } = Color.Moccasin;
+
+        [DisplayName("Background Color")]
+        [Description("The color used for overall background.")]
+        [Browsable(true)]
+        [JsonConverter(typeof(JsonColorConverter))]
+        public Color BackColor { get; set; } = Color.LightYellow;
+        #endregion
+
+        #region Properties - internal
+        [Browsable(false)]
+        public bool Valid { get; set; } = false;
+
+        [Browsable(false)]
+        public bool WordWrap { get; set; } = false;
+
+        [Browsable(false)]
+        public bool MonitorInput { get; set; } = false;
+
+        [Browsable(false)]
+        public bool MonitorOutput { get; set; } = false;
+        #endregion
+    }
+}
