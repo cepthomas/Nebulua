f7c01bdb609e17f9a1a99bb975088e882cd2aee5
cepthomas <cepthomas@gmail.com>
2025-03-17 17:37:08 -0500

    WIP.

--- MainForm.cs
+++ MainForm.cs
@@ -63,105 +63,8 @@ namespace Nebulua
             // TODO1 When running in VS: If the controls are inited here (as they should)
             // a cross-thread access exception may be thrown. Doesn't happen if the code is
             // moved to OnLoad(). Doesn't happen if running the exe from the cli. Very strange.
-            // Also VS is very slow to startup the app (and shutdown). Again not from the cli.
+            // Also VS is very slow to startup the app (and shutdown). Again not from the cli or running the exe.
 
-            //PopulateFileMenu();
-
-            //_watcher.NotifyFilter = NotifyFilters.LastWrite;
-            //_watcher.Changed += Watcher_Changed;
-
-            //#region Init the controls
-            //timeBar.BackColor = UserSettings.Current.BackColor;
-            //timeBar.ProgressColor = UserSettings.Current.ControlColor;
-            //timeBar.MarkerColor = Color.Black;
-
-            //chkPlay.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkPlay.Image!, UserSettings.Current.ForeColor);
-            //chkPlay.BackColor = UserSettings.Current.BackColor;
-            //chkPlay.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
-            //chkPlay.Click += Play_Click;
-
-            //chkLoop.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkLoop.Image!, UserSettings.Current.ForeColor);
-            //chkLoop.BackColor = UserSettings.Current.BackColor;
-            //chkLoop.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
-            //chkLoop.Click += (_, __) => State.Instance.DoLoop = chkLoop.Checked;
-
-            //chkMonRcv.BackColor = UserSettings.Current.BackColor;
-            //chkMonRcv.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkMonRcv.Image!, UserSettings.Current.ForeColor);
-            //chkMonRcv.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
-            //chkMonRcv.Checked = UserSettings.Current.MonitorRcv;
-            //chkMonRcv.Click += (_, __) => UserSettings.Current.MonitorRcv = chkMonRcv.Checked;
-
-            //chkMonSnd.BackColor = UserSettings.Current.BackColor;
-            //chkMonSnd.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkMonSnd.Image!, UserSettings.Current.ForeColor);
-            //chkMonSnd.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
-            //chkMonSnd.Checked = UserSettings.Current.MonitorSnd;
-            //chkMonSnd.Click += (_, __) => UserSettings.Current.MonitorSnd = chkMonSnd.Checked;
-
-            //btnRewind.BackColor = UserSettings.Current.BackColor;
-            //btnRewind.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnRewind.Image!, UserSettings.Current.ForeColor);
-            //btnRewind.Click += Rewind_Click;
-
-            //btnAbout.BackColor = UserSettings.Current.BackColor;
-            //btnAbout.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnAbout.Image!, UserSettings.Current.ForeColor);
-            //btnAbout.Click += About_Click;
-
-            //btnKill.BackColor = UserSettings.Current.BackColor;
-            //btnKill.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnKill.Image!, UserSettings.Current.ForeColor);
-            //btnKill.Click += (_, __) => { _core!.KillAll(); State.Instance.ExecState = ExecState.Idle; };
-
-            //btnSettings.BackColor = UserSettings.Current.BackColor;
-            //btnSettings.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnSettings.Image!, UserSettings.Current.ForeColor);
-            //btnSettings.Click += Settings_Click;
-
-            //sldVolume.BackColor = UserSettings.Current.BackColor;
-            //sldVolume.DrawColor = UserSettings.Current.ControlColor;
-            //sldVolume.ValueChanged += (_, __) => State.Instance.Volume = sldVolume.Value;
-
-            //sldTempo.BackColor = UserSettings.Current.BackColor;
-            //sldTempo.DrawColor = UserSettings.Current.ControlColor;
-            //sldTempo.ValueChanged += (_, __) => State.Instance.Tempo = (int)sldTempo.Value;
-
-            //traffic.BackColor = UserSettings.Current.BackColor;
-            //traffic.MatchText.Add("ERR", Color.HotPink);
-            //traffic.MatchText.Add("WRN", Color.Coral);
-            //traffic.MatchText.Add("SND", Color.PaleGreen);
-            //traffic.MatchText.Add("RCV", Color.LightBlue);
-            //traffic.Font = new("Cascadia Mono", 9);
-            //traffic.Prompt = "";
-            //traffic.WordWrap = UserSettings.Current.WordWrap;
-            ////traffic.Clear();
-
-            //ccMidiGen.Name = "ClickClack";
-            //ccMidiGen.MinX = 24; // C0
-            //ccMidiGen.MaxX = 96; // C6
-            //ccMidiGen.GridX = [12, 24, 36, 48, 60, 72, 84];
-            //ccMidiGen.MinY = 0; // min velocity == note off
-            //ccMidiGen.MaxY = 127; // max velocity
-            //ccMidiGen.GridY = [32, 64, 96];
-            //ccMidiGen.MouseClickEvent += CcMidiGen_MouseClickEvent;
-            //ccMidiGen.MouseMoveEvent += CcMidiGen_MouseMoveEvent;
-
-            //ddbtnFile.Image = GraphicsUtils.ColorizeBitmap((Bitmap)ddbtnFile.Image!, UserSettings.Current.ForeColor);
-            //ddbtnFile.BackColor = UserSettings.Current.BackColor;
-            //ddbtnFile.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
-            //ddbtnFile.Enabled = true;
-            //ddbtnFile.Selected += File_Selected;
-            //#endregion
-
-            //btnGo.Click += (_, __) => Console.WriteLine("<<<<<< GOGOGOGO >>>>>");
-
-            //// Now ready to go live.
-            //State.Instance.ValueChangeEvent += State_ValueChangeEvent;
-            /////////////////////////////////////////////////////////////////////////
-        }
-
-        /// <summary>
-        /// Inits control appearance. Opens script. Can throw.
-        /// </summary>
-        /// <param name="e"></param>
-        protected override void OnLoad(EventArgs e)
-        {
-            /////////////////////////////////////////////////////////////////////////
             PopulateFileMenu();
 
             _watcher.NotifyFilter = NotifyFilters.LastWrite;
@@ -246,6 +149,103 @@ namespace Nebulua
             #endregion
 
             btnGo.Click += (_, __) => Console.WriteLine("<<<<<< GOGOGOGO >>>>>");
+
+            //// Now ready to go live.
+            //State.Instance.ValueChangeEvent += State_ValueChangeEvent;
+            /////////////////////////////////////////////////////////////////////////
+        }
+
+        /// <summary>
+        /// Inits control appearance. Opens script. Can throw.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            /////////////////////////////////////////////////////////////////////////
+            //PopulateFileMenu();
+
+            //_watcher.NotifyFilter = NotifyFilters.LastWrite;
+            //_watcher.Changed += Watcher_Changed;
+
+            //#region Init the controls
+            //timeBar.BackColor = UserSettings.Current.BackColor;
+            //timeBar.ProgressColor = UserSettings.Current.ControlColor;
+            //timeBar.MarkerColor = Color.Black;
+
+            //chkPlay.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkPlay.Image!, UserSettings.Current.ForeColor);
+            //chkPlay.BackColor = UserSettings.Current.BackColor;
+            //chkPlay.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
+            //chkPlay.Click += Play_Click;
+
+            //chkLoop.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkLoop.Image!, UserSettings.Current.ForeColor);
+            //chkLoop.BackColor = UserSettings.Current.BackColor;
+            //chkLoop.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
+            //chkLoop.Click += (_, __) => State.Instance.DoLoop = chkLoop.Checked;
+
+            //chkMonRcv.BackColor = UserSettings.Current.BackColor;
+            //chkMonRcv.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkMonRcv.Image!, UserSettings.Current.ForeColor);
+            //chkMonRcv.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
+            //chkMonRcv.Checked = UserSettings.Current.MonitorRcv;
+            //chkMonRcv.Click += (_, __) => UserSettings.Current.MonitorRcv = chkMonRcv.Checked;
+
+            //chkMonSnd.BackColor = UserSettings.Current.BackColor;
+            //chkMonSnd.Image = GraphicsUtils.ColorizeBitmap((Bitmap)chkMonSnd.Image!, UserSettings.Current.ForeColor);
+            //chkMonSnd.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
+            //chkMonSnd.Checked = UserSettings.Current.MonitorSnd;
+            //chkMonSnd.Click += (_, __) => UserSettings.Current.MonitorSnd = chkMonSnd.Checked;
+
+            //btnRewind.BackColor = UserSettings.Current.BackColor;
+            //btnRewind.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnRewind.Image!, UserSettings.Current.ForeColor);
+            //btnRewind.Click += Rewind_Click;
+
+            //btnAbout.BackColor = UserSettings.Current.BackColor;
+            //btnAbout.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnAbout.Image!, UserSettings.Current.ForeColor);
+            //btnAbout.Click += About_Click;
+
+            //btnKill.BackColor = UserSettings.Current.BackColor;
+            //btnKill.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnKill.Image!, UserSettings.Current.ForeColor);
+            //btnKill.Click += (_, __) => { _core!.KillAll(); State.Instance.ExecState = ExecState.Idle; };
+
+            //btnSettings.BackColor = UserSettings.Current.BackColor;
+            //btnSettings.Image = GraphicsUtils.ColorizeBitmap((Bitmap)btnSettings.Image!, UserSettings.Current.ForeColor);
+            //btnSettings.Click += Settings_Click;
+
+            //sldVolume.BackColor = UserSettings.Current.BackColor;
+            //sldVolume.DrawColor = UserSettings.Current.ControlColor;
+            //sldVolume.ValueChanged += (_, __) => State.Instance.Volume = sldVolume.Value;
+
+            //sldTempo.BackColor = UserSettings.Current.BackColor;
+            //sldTempo.DrawColor = UserSettings.Current.ControlColor;
+            //sldTempo.ValueChanged += (_, __) => State.Instance.Tempo = (int)sldTempo.Value;
+
+            //traffic.BackColor = UserSettings.Current.BackColor;
+            //traffic.MatchText.Add("ERR", Color.HotPink);
+            //traffic.MatchText.Add("WRN", Color.Coral);
+            //traffic.MatchText.Add("SND", Color.PaleGreen);
+            //traffic.MatchText.Add("RCV", Color.LightBlue);
+            //traffic.Font = new("Cascadia Mono", 9);
+            //traffic.Prompt = "";
+            //traffic.WordWrap = UserSettings.Current.WordWrap;
+            ////traffic.Clear();
+
+            //ccMidiGen.Name = "ClickClack";
+            //ccMidiGen.MinX = 24; // C0
+            //ccMidiGen.MaxX = 96; // C6
+            //ccMidiGen.GridX = [12, 24, 36, 48, 60, 72, 84];
+            //ccMidiGen.MinY = 0; // min velocity == note off
+            //ccMidiGen.MaxY = 127; // max velocity
+            //ccMidiGen.GridY = [32, 64, 96];
+            //ccMidiGen.MouseClickEvent += CcMidiGen_MouseClickEvent;
+            //ccMidiGen.MouseMoveEvent += CcMidiGen_MouseMoveEvent;
+
+            //ddbtnFile.Image = GraphicsUtils.ColorizeBitmap((Bitmap)ddbtnFile.Image!, UserSettings.Current.ForeColor);
+            //ddbtnFile.BackColor = UserSettings.Current.BackColor;
+            //ddbtnFile.FlatAppearance.CheckedBackColor = UserSettings.Current.SelectedColor;
+            //ddbtnFile.Enabled = true;
+            //ddbtnFile.Selected += File_Selected;
+            //#endregion
+
+            //btnGo.Click += (_, __) => Console.WriteLine("<<<<<< GOGOGOGO >>>>>");
 
             // Now ready to go live.
             State.Instance.ValueChangeEvent += State_ValueChangeEvent;
