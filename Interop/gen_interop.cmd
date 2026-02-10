385a99cff7e0de72f2c1da00a84b70bb69d57220
cepthomas <cepthomas@gmail.com>
2026-02-10 17:55:08 -0600

    Clean up

--- Interop/gen_interop.cmd
+++ Interop/gen_interop.cmd
@@ -1,16 +1,17 @@
 :: Convert spec into interop library.
 :: Should not need to be regenerated after the api is finalized.
 :: Note this is specific to my configuration only.
+:: Requires env var DEV_PATH to be set.
 
 echo off
 cls
 
-set ODIR=%cd%
+set INTEROP_DIR=%cd%
 cd ..\
 set LBOT_DIR=%cd%\LBOT
-set LUA_PATH=%LBOT_DIR%\?.lua;%ODIR%\?.lua;?.lua;
+set LUA_PATH=%LBOT_DIR%\?.lua;%INTEROP_DIR%\?.lua;?.lua;
 cd %DEV_PATH%\Libs\LuaInterop\Generator
 :: Gen the C and C++ components from the spec.
-lua do_gen.lua -c %ODIR%\interop_spec.lua %ODIR%
-lua do_gen.lua -cppcli %ODIR%\interop_spec.lua %ODIR%
-cd %ODIR%
+lua do_gen.lua -c %INTEROP_DIR%\interop_spec.lua %INTEROP_DIR%
+lua do_gen.lua -cppcli %INTEROP_DIR%\interop_spec.lua %INTEROP_DIR%
+cd %INTEROP_DIR%
