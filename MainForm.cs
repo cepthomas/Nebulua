c9760fdd2c03c6705de0c6264a470ba494223737
cepthomas <cepthomas@gmail.com>
2026-05-30 11:27:50 -0500

    Housekeeping.

--- MainForm.cs
+++ MainForm.cs
@@ -14,9 +14,6 @@ using Ephemera.MidiLib;
 using Ephemera.MusicLib;
 
 
-// TODO1 remove? "x64": Apps\Nebulua, Libs\LuaInterop. Some breakage using py script.
-
-
 namespace Nebulua
 {
     public partial class MainForm : Form
